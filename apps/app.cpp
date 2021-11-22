d9d8d47dd6a178354fffefefb3a7551622543ca0
Mudit Gupta <muditg317@gmail.com>
2021-11-22 11:25:45 -0500

    add horizontal bouncing

--- apps/app.cpp
+++ apps/app.cpp
@@ -13,16 +13,16 @@
 Eigen::Vector2f circlePos;
 Eigen::Vector2f circleVel;
 const Eigen::Vector2f gravity(0,0.00015);
-float r = 30;
+float r = 10;
 
 void reset() {
   circlePos = Eigen::Vector2f(64,30);
-  circleVel = Eigen::Vector2f(0,0);
+  circleVel = Eigen::Vector2f(0.1,0);
 }
 
 void setup() {
-  display_size(128,128);
-  frameRate(30);
+  display_size(256,128);
+  frameRate(100);
   reset();
 }
 #define restitution 1.0
@@ -36,6 +36,16 @@ void update() {
     circleVel.y() *= -restitution;
     circlePos.y() *= bounceDamp-1;
     circlePos.y() += (2-bounceDamp)*(HEIGHT - r);
+  }
+  if (circlePos.x() >= WIDTH - r) {
+    circleVel.x() *= -restitution;
+    circlePos.x() *= bounceDamp-1;
+    circlePos.x() += (2-bounceDamp)*(WIDTH - r);
+  }
+  if (circlePos.x() <= r) {
+    circleVel.x() *= -restitution;
+    circlePos.x() *= bounceDamp-1;
+    circlePos.x() += (2-bounceDamp)*(r);
   }
   circle(circlePos,r);
   // printf("Buffer: %d\n", BUFFER[WIDTH/2][HEIGHT/2]);
